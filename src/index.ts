621e3b4008decc9ad740a811f0f17b0e15ff9f1e
ruritoBlogger <rurito.blog@gmail.com>
2021-08-15 14:25:26 +0900

    feat: Add storybook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { CreateDriftingAnimation } from "./CreateDriftingAnimation";
 export { CreateJumpAnimation } from "./CreateJumpAnimation";
-export { CreateLoadingAnimation } from "./CreateLoadingAnimation";
+export { CreateLoadingAnimation } from "./CreateLoadingAnimation/animation";
 export { CreateMovingWindowFromRightAnimation } from "./CreateMovingWindowFromRightAnimation";
 export { CreateShakeAndUpAnimation } from "./CreateShakeAndUpAnimation";
